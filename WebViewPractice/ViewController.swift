200726a376d00ce176eb02667745e6762d4cb785
Takahiro Hara <takahiro.hb@gmail.com>
2020-01-08 01:14:01 +0900

    Initial Commit

---
+++ WebViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewPractice
+//
+//  Created by 原昂大 on 2020/01/08.
+//  Copyright © 2020 takahiro.hara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
